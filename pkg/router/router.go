1de6e4c3d4da63f9ed34747032bde3c049c6f8c7
Shrish <shrishmak986@gmail.com>
2025-09-19 18:19:17 +0530

    changed /api to /

--- pkg/router/router.go
+++ pkg/router/router.go
@@ -17,7 +17,7 @@ func RegisterRoute(e *echo.Echo) {
 	e.POST("/callback", controllers.CallbackUrl)
 
 	// API group with JWT authentication
-	api := e.Group("/api")
+	api := e.Group("/")
 	api.Use(middlewares.VerifyJWTMiddleware)
 
 	// Authenticated user routes
