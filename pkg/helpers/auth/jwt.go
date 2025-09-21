4e735ae6b040492efb17ad0bb69c7af06ea1a3c4
RustyDev <vedant.matanhelia@gmail.com>
2025-09-21 15:16:38 +0000

    pushing another fix

--- pkg/helpers/auth/jwt.go
+++ pkg/helpers/auth/jwt.go
@@ -1,14 +1,11 @@
 package auth
 
 import (
-	"fmt"
 	"time"
 
 	"github.com/CodeChefVIT/cookoff-10.0-be/pkg/db"
 	"github.com/CodeChefVIT/cookoff-10.0-be/pkg/utils"
 	"github.com/golang-jwt/jwt/v5"
-	"github.com/google/uuid"
-	"github.com/labstack/echo/v4"
 )
 
 var jwtSecret = []byte(utils.Config.JwtSecret)
@@ -55,30 +52,4 @@ func CreateRefreshToken(user *db.User) (string, error) {
 
 	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
 	return token.SignedString(jwtSecret)
-}
-
-func GetUserID(c echo.Context) (uuid.UUID, error) {
-	user := c.Get("user").(*jwt.Token)
-
-	if user == nil {
-		return uuid.UUID{}, fmt.Errorf("JWT Token not found")
-	}
-
-	claims := user.Claims.(jwt.MapClaims)
-
-	if !user.Valid {
-		return uuid.UUID{}, fmt.Errorf("the JWT Token is invalid")
-	}
-
-	userIDStr, ok := claims["user_id"].(string)
-	if !ok {
-		return uuid.UUID{}, fmt.Errorf("user_id not found in token")
-	}
-
-	uid, err := uuid.Parse(userIDStr)
-	if err != nil {
-		return uuid.UUID{}, fmt.Errorf("invalid user_id format: %v", err)
-	}
-
-	return uid, nil
 }
