665dc675939797ebf2c9ce4fd8b96fd9538e8207
Atharva Sharma <atharva17asm@gmail.com>
2025-09-21 22:56:41 +0530

    Update: added auth to submit code

--- pkg/dto/submissions.go
+++ pkg/dto/submissions.go
@@ -4,7 +4,6 @@ type SubmissionRequest struct {
 	SourceCode string `json:"source_code" validate:"required"`
 	LanguageID int    `json:"language_id" validate:"required"`
 	QuestionID string `json:"question_id" validate:"required"`
-	UserID     string `json:"user_id"`
 }
 type SubmissionPayload struct {
 	LanguageID int     `json:"language_id"`
