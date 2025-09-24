d52668a04d9c3ae19d7b4ccb50e1f1124a79434f
Lavnish <lavjhn@gmail.com>
2025-09-24 22:44:16 +0530

    yes

--- pkg/dto/dashboard_response.go
+++ pkg/dto/dashboard_response.go
@@ -1,21 +1,12 @@
 package dto
 
 type DashboardResponse struct {
-	UserName               string `json:"user_name"`
-	Email                  string `json:"email"`
-	QuestionsCompleted0    int64  `json:"questions_completed_round0"`
-	QuestionsNotCompleted0 int64  `json:"questions_not_completed_round0"`
-	QuestionsCompleted1    int64  `json:"questions_completed_round1"`
-	QuestionsNotCompleted1 int64  `json:"questions_not_completed_round1"`
-	QuestionsCompleted2    int64  `json:"questions_completed_round2"`
-	QuestionsNotCompleted2 int64  `json:"questions_not_completed_round2"`
-	QuestionsCompleted3    int64  `json:"questions_completed_round3"`
-	QuestionsNotCompleted3 int64  `json:"questions_not_completed_round3"`
-	Round0Score            int32  `json:"round_0_score"`
-	Round1Score            int32  `json:"round_1_score"`
-	Round2Score            int32  `json:"round_2_score"`
-	Round3Score            int32  `json:"round_3_score"`
-	CurrentRound           int8   `json:"current_round"`
+	UserName              string   `json:"username"`
+	Email                 string   `json:"email"`
+	QuestionsCompleted    [4]int32 `json:"questionsCompleted"`
+	QuestionsNotCompleted [4]int32 `json:"questionnsNotCompleted"`
+	RoundScores           [4]int32 `json:"roundScores"`
+	CurrentRound          int8     `json:"currentRound"`
 }
 
 type RoundPoints struct {
