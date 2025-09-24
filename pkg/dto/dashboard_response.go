7ed007fb859b6dd4a7642dc77422dbb41e09ad49
Lavnish <lavjhn@gmail.com>
2025-09-24 23:09:10 +0530

    fixed

--- pkg/dto/dashboard_response.go
+++ pkg/dto/dashboard_response.go
@@ -3,10 +3,10 @@ package dto
 type DashboardResponse struct {
 	UserName              string   `json:"username"`
 	Email                 string   `json:"email"`
-	QuestionsCompleted    [4]int32 `json:"questionsCompleted"`
-	QuestionsNotCompleted [4]int32 `json:"questionnsNotCompleted"`
-	RoundScores           [4]int32 `json:"roundScores"`
-	CurrentRound          int8     `json:"currentRound"`
+	QuestionsCompleted    [4]int32 `json:"questions_completed"`
+	QuestionsNotCompleted [4]int32 `json:"questions_not_completed"`
+	RoundScores           [4]int32 `json:"round_scores"`
+	CurrentRound          int8     `json:"current_round"`
 }
 
 type RoundPoints struct {
